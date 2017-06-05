663d3fc9d0865d326bb5ccd00db2c280f2adc2a9
Zach S <zsiegel92@gmail.com>
2017-06-05 12:00:03 -0700

    changed sqlalchemy track mod

--- src/config.py
+++ src/config.py
@@ -17,6 +17,7 @@ class Config(object):
 class ProductionConfig(Config):
 	APP_NAME="ZachAndFriends"
 	DEBUG = False
+	SQLALCHEMY_TRACK_MODIFICATIONS = False
 	MESSENGER_PLATFORM_ACCESS_TOKEN='EAAbL0Ffr9PkBADy64t821NSnuMRQx8U1Tm0FAXgS5840g7bm2ryZAHVo4YP74O5mOuZCERJ5mXxaDi7aaFC8RjHM9B4a1YGaba3do6ZAkxX5ci9ncGIH8KuHx2UJoBzUdAuuCWamNJXSbbxGdxqJoUpPoCyGEBYjTx4RdczFwZDZD' #ZachNFriends
 
 #NOTE: I can change this to:

0c56c2e7b08cc593d0b62199f2592a0b7c0c3d5e
Zach S <zsiegel92@gmail.com>
2017-08-05 21:42:33 -0700

    try and except in GTresult

--- src/pageviews.py
+++ src/pageviews.py
@@ -333,21 +333,27 @@ def GT_results(job_key):
 		job = Job.fetch(job_key, connection=conn)
 	except:
 		return "No such job",200
-	try:
-		if job.is_finished:
-			params=job.result
-			print("Job finished in GT_results: " + str(params),file=sys.stderr)
+
+	if job.is_finished:
+		params=job.result
+		print("Job finished in GT_results: " + str(params),file=sys.stderr)
+		try:
 			output = [[('names',ass['names']),('isPossible',ass['isPossible']),('notLatePossible',ass['notLatePossible']),('lateOk',ass['lateOk']),('bestTimes',ass['bestTimes'])] for ass in params.solution['assignments']]
 			output = [[ (tup[0],np.asscalar(tup[1]) if isinstance(tup[1],np.generic) else tup[1]) for tup in piece] for piece in output] #Convert np.bool_ to bool, eg
 			# output = [{'names':ass['names'],'bestTimes':ass['bestTimes']} for ass in params.solution['assignments']]
-			print("output before jsonify: " + str(output),file=sys.stderr)
+		except Exception as exc:
+			return "Something bad happened BEFORE jsonify " + str(exc),500
+
+		print("output before jsonify: " + str(output),file=sys.stderr)
+		try:
 			a=jsonify(output)
 			print("successfully jsonified output!",file=sys.stderr)
 			return a,200
-		else:
-			# print("PROBLEM IS HERE?!")
-			return "Still working on GROUPTHERE!!", 202
-	except Exception as exc:
-		return "Something bad happened" + str(exc),500
+		except Exception as exc:
+			return "Something bad happened DURING jsonify " + str(exc),500
+	else:
+		# print("PROBLEM IS HERE?!")
+		return "Still working on GROUPTHERE!!", 202
+
 
 

d96d831d0f438f954b792df01c397131a4bd1106
Zach S <zsiegel92@gmail.com>
2017-08-07 00:42:48 -0700

    built out cron stuff

--- src/cron_email_test.py
+++ src/cron_email_test.py
@@ -8,12 +8,19 @@ from flask_sqlalchemy import SQLAlchemy
 q = Queue(connection=conn) #INTERACTIONS MOVEMENT!
 
 
-def send_email(sent_to,sent_from,password,message):
+def send_email(sent_to,sent_from,password,message="",subject=""):
 	try:
+		fullMessage = "\r\n".join([
+			"From: " + sent_from,
+			"To: " + sent_to,
+			"Subject: " + subject,
+			"",
+			message
+		])
 		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 		server.ehlo()
 		server.login(sent_from, password)
-		server.sendmail(sent_from, sent_to, message)
+		server.sendmail(sent_from, sent_to, fullMessage)
 		server.close()
 		# messenger.say(recipient_id,"You just got an email from GroupThere!")
 	except Exception as exc:
@@ -24,9 +31,10 @@ def send_email(sent_to,sent_from,password,message):
 
 def send_test_email(toAddress):
 	message = "SENDING A TEST EMAIL FROM CRON JOB"
+	subject = "a message from cron!"
 	gmail_user =getattr(config,os.environ['APP_SETTINGS'].split('.')[1]).EMAIL
 	gmail_password=getattr(config,os.environ['APP_SETTINGS'].split('.')[1]).EMAIL_PASSWORD
-	send_email(toAddress,gmail_user,gmail_password,message)
+	send_email(toAddress,gmail_user,gmail_password,message,subject)
 
 
 

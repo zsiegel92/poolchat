72d469de41009367dc68e0b8d25f8e61f4ba5ca7
Zach S <zsiegel92@gmail.com>
2017-08-07 00:26:07 -0700

    cron scheduled task

--- src/cron_email_test.py
+++ src/cron_email_test.py
@@ -8,23 +8,27 @@ from flask_sqlalchemy import SQLAlchemy
 q = Queue(connection=conn) #INTERACTIONS MOVEMENT!
 
 
-
-def send_test_email(toAddress):
-	message = "SENDING A TEST EMAIL FROM CRON JOB"
-	gmail_user =getattr(config,os.environ['APP_SETTINGS'].split('.')[1]).EMAIL
-	gmail_password=getattr(config,os.environ['APP_SETTINGS'].split('.')[1]).EMAIL_PASSWORD
-	sent_from = gmail_user
-	sent_to = toAddress
+def send_email(sent_to,sent_from,password,message):
 	try:
 		server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 		server.ehlo()
-		server.login(gmail_user, gmail_password)
+		server.login(sent_from, password)
 		server.sendmail(sent_from, sent_to, message)
 		server.close()
 		# messenger.say(recipient_id,"You just got an email from GroupThere!")
 	except Exception as exc:
 		print('Something went wrong with login.')
 		print("exception: " + str(exc))
+
+
+
+def send_test_email(toAddress):
+	message = "SENDING A TEST EMAIL FROM CRON JOB"
+	gmail_user =getattr(config,os.environ['APP_SETTINGS'].split('.')[1]).EMAIL
+	gmail_password=getattr(config,os.environ['APP_SETTINGS'].split('.')[1]).EMAIL_PASSWORD
+	send_email(toAddress,gmail_user,gmail_password,message)
+
+
 
 if __name__ == '__main__':
 	print("Trying to send email")
